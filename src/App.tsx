27e1759e4d6dbabc32179525e133385fb52abfcb
Mohammad Bakhshi <77542557+mohammad-bakhshi@users.noreply.github.com>
2023-09-21 18:51:11 +0430

    add iranyekan as global font family in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-body-xl font-bold m-l underline text-grape-primary font-iran-yekan">
+      <h1 className="text-body-xl font-bold m-l underline text-grape-primary">
         فونت ایران‌یکان
       </h1>
-      {/* <Button
-        onClick={() => console.log("test")}
-        title="test"
-        className="bg-red-primary w-l h-l"
-      /> */}
     </div>
   );
 }
