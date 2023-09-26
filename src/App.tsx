521f37c5286175d4a6125924b14ddc1d302c7eec
Zahra <zahra.reh1998@gmal.com>
2023-09-27 00:09:51 +0330

    create modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { BiSearch } from "react-icons/bi";
 import Design from "./components/Design";
 import Input from "./components/Input";
+import Modal from "./components/Modal";
 function App() {
   return (
     <div className="App">
-      {/* <Design></Design> */}
-      <Input
-        type="text"
-        value="سلام"
-        id="test"
-        icon={<BiSearch />}
-        label={{ text: "test", for: "test" }}
-      />
+      <Modal
+        modalTitle="test"
+        buttonTitle="test"
+        currentPage={2}
+        totalPages={5}
+        modalClassname="w-[500px]"
+      >
+        <p>test</p>
+      </Modal>
     </div>
   );
 }
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,109 @@
+import { MdClose, MdArrowBack } from "react-icons/md";
+import Button from "../Button";
+import { IoEllipse } from "react-icons/io5";
+import { useState } from "react";
+interface IModalProbs {
+  modalTitle: string;
+  // isOpen: boolean;
+  onClick?: () => void; //Function for footer button
+  //   onClose: () => void; //Fonction to close modal
+  onPrevPage?: () => void; //funcion to go previous modal page
+  currentPage: number;
+  totalPages: number; //Number of modal page
+  children: React.ReactNode; //Content of modal's body
+  buttonTitle: string; //Content of footer button
+  modalClassname: string;
+  buttonClassName?: string;
+}
+
+const Modal: React.FC<IModalProbs> = ({
+  //   isOpen,
+  //   onClose,
+  onPrevPage,
+  onClick,
+  modalTitle,
+  currentPage,
+  totalPages,
+  children,
+  buttonTitle,
+  modalClassname,
+  buttonClassName,
+}): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const renderPageButtons = () => {
+    const buttons = [];
+
+    for (let page = 1; page <= totalPages; page++) {
+      const isactive = page === currentPage;
+
+      buttons.push(
+        <button
+          key={page}
+          className="text-red-800 w-auto h-[8px]"
+          onClick={() => onPageClick(page)}
+        >
+          {isactive ? (
+            <IoEllipse className="w-[8px] h-[8px]" color="#FFFFFF" />
+          ) : (
+            <IoEllipse className="w-[8px] h-[8px]" color="#8A8989" />
+          )}
+        </button>
+      );
+    }
+
+    return buttons;
+  };
+  const onPageClick = (page: number) => {};
+  return (
+    <div
+      className={` top-0 left-0  h-full flex  flex-col items-center justify-around rounded-lg gap-l z-50 relative p-[20px] ${
+        isOpen ? "block" : "hidden"
+      } ${modalClassname}`}
+    >
+      <div
+        className={` top-0 left-0 w-full h-full flex  flex-col items-center justify-between rounded-lg bg-gray-secondary gap-l  relative p-[20px] ${
+          isOpen ? "block" : "hidden"
+        } `}
+      >
+        {/* Header content goes here */}
+        <div className="flex justify-center w-full">
+          <button
+            className="text-gray-800 w-[24px] h-[24px] absolute right-s "
+            onClick={() => setIsOpen(false)}
+          >
+            <MdClose /> {/* Close button with the close icon */}
+          </button>
+          <h2 className="text-[24px] font-semibold self-center">
+            {modalTitle}
+          </h2>
+
+          {totalPages > 1 && currentPage > 1 && (
+            <button
+              className="text-gray-800 w-[24px] h-[24px] absolute left-xs"
+              onClick={onPrevPage}
+            >
+              <MdArrowBack /> {/* Previous Page button with the arrow icon */}
+            </button>
+          )}
+        </div>
+        {/* Body content goes here  */}
+        <div>{children}</div>
+        {/* Footer content goes here */}
+        <div className="w-full flex justify-center px-[24px]">
+          <Button
+            type="button"
+            disabled
+            className={` flex justify-center items-start w-full h-[40px] rounded-md p-[10px] text-[14px] text-gray-secondary bg-brand-primary font-extrabold ${buttonClassName}`}
+            onClick={onClick}
+            title={buttonTitle}
+          ></Button>
+        </div>
+      </div>
+      <div className="flex gap-[8px] justify-center">
+        {totalPages > 1 && renderPageButtons()}
+      </div>
+    </div>
+  );
+};
+export default Modal;
