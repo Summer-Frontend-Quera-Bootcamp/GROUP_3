00494b9518e979e0f5c446c7f34858747460f327
Ali Zarei <ali.zarei0105@gmail.com>
2023-09-30 18:29:32 +0330

    #

---
+++ src/components/Changedate/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Button from '../Button'
+import { BsChevronLeft , BsChevronRight } from "react-icons/bs";
+
+const Changedate: React.FC = () => {
+  return (
+    <div className='w-[200px]  h-[24] gap-xs flex'>
+      <div className='w-[30px] h-[21px] font-iran-yekan body-xs self-start '>امروز</div>
+      <div className='w-[30px] h-[24px] flex'>
+      <Button
+              title=""
+              disabled={false}
+              onClick={() => (0)}
+              icon={<BsChevronRight/>}
+            />
+            <Button
+            title=""
+            disabled={false}
+            onClick={() => (0)}
+            icon={<BsChevronLeft/>}
+            />
+      </div>
+      <div className='w-[150px] h-[21px] body-xs font-iran-yekan self-start flex'>اردیبهشت ۱۴۰۲</div>
+    </div>
+  )
+}
+
+export default Changedate
