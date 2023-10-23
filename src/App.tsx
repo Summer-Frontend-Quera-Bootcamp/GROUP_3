3c835e8ecc625c86eb89df4bbd50805e94b4e223
Amirzr21 <amirzaree701@gmail.com>
2023-10-23 18:43:04 +0330

    #

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Account from "./components/Account";
 import AccountForm from "./components/AccountForm";
 import Design from "./components/Design";
+import TaskBoard from "./components/TaskManeger/TaskBoard";
 function App() {
   return (
     <div className="App">
       {/* <Design></Design> */}
-      <Account />
+      {/* <Account /> */}
+      <TaskBoard></TaskBoard>
     </div>
   );
 }
